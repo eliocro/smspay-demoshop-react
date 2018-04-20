62de7efe706081c161ab9164ad5080185a054a60
Élio Cró <elio@front.no>
2018-04-20 16:43:40 +0100

    Small css fixes

--- src/components/StoreFront.js
+++ src/components/StoreFront.js
@@ -9,9 +9,10 @@ class StoreFront extends Component {
   render () {
     return (
       <section className="container">
-        <h2>The best albums</h2>
-        <h4>Buy them here!</h4>
-
+        <div style={{ paddingLeft: '10px' }}>
+          <h2>The best albums</h2>
+          <h4>Buy them here!</h4>
+        </div>
         <ul className="products">
           { products.map(p => (
             <AlbumItem album={ p } key={ p.id } history={ this.props.history } />
